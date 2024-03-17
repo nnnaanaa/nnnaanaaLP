ce63d86cf7699311c2fd982e87728061d800c1d2
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2024-03-17 22:05:52 +0900

    scbk

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+
+const scbk_point = 200
+
 window.addEventListener("scroll", function () {
     // ヘッダーを変数の中に格納する
     const top_border = document.querySelector(".top-border");
     const bottom_border = document.querySelector(".bottom-border");
+    const nav_menu = document.querySelector(".nav-menu");
     // 100px以上スクロールしたらヘッダーに「scroll」クラスをつける
-    top_border.classList.toggle("scroll", window.scrollY > 100);
-    bottom_border.classList.toggle("scroll", window.scrollY > 100);
-  });
+    top_border.classList.toggle("scroll", window.scrollY > scbk_point);
+    bottom_border.classList.toggle("scroll", window.scrollY > scbk_point);
+    nav_menu.classList.toggle("scroll", window.scrollY > scbk_point);
+});
